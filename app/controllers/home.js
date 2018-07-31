561172303076e95775cc103bc6143067ef7dbe1c
umashankar <umashankar.somasekar@rapidqube.com>
2018-07-31 15:32:40 +0530

    added host to env

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,7 +5,11 @@ export default Controller.extend({
     actions : {
         redirect : function(routename){
             if(routename==='myissue'){
-                window.location.href = 'https://rapidqubedigital.sharepoint.com/Lists/IT%20Helpdesk/My%20Tickets.aspx'
+                window.open(
+                    'https://rapidqubedigital.sharepoint.com/Lists/IT%20Helpdesk/My%20Tickets.aspx',
+                    '_blank' // <- This is what makes it open in a new window.
+                  );
+               
             }
             else{
                 this.transitionToRoute(routename)
